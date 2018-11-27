43582ce40bec2e5d56b1ccbfc2b20dba174f310e
howiezhao <howiezhaohr@hotmail.com>
2018-11-27 22:12:39 +0800

    Add project files.

---
+++ AKUOJ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AKUOJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
