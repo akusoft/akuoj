bfab4ffbb0038409fb3893255c744b062199e0dc
howiezhao <howiezhaohr@hotmail.com>
2020-04-27 19:52:29 +0800

    refact: migrate to spring

---
+++ src/test/java/com/howie/akuoj/AkuojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howie.akuoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkuojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
