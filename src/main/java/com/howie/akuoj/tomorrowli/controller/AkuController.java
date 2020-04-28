50ef943056260343a4393c84ea840acb65e80036
Tomorrowli <1425560783@qq.com>
2020-04-28 18:34:55 +0800

    解决thymeleaf问题

--- src/main/java/com/howie/akuoj/tomorrowli/controller/AkuController.java
+++ src/main/java/com/howie/akuoj/tomorrowli/controller/AkuController.java
@@ -1,7 +1,14 @@
 package com.howie.akuoj.tomorrowli.controller;
 
+import com.howie.akuoj.tomorrowli.dto.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: LiMing
@@ -13,7 +20,15 @@ import org.springframework.web.bind.annotation.*;
 public class AkuController {
 
     @RequestMapping(value = "/hello")
-    public String hello() {
+    public String hello(Model model) {
+        List<User> userlist = new ArrayList<>();
+        User user = new User();
+        user.setName("张三");
+        User user1 = new User();
+        user1.setName("李四");
+        userlist.add(user);
+        userlist.add(user1);
+        model.addAttribute("list",userlist);
         return "/index";
     }
 }
---
+++ src/main/java/com/howie/akuoj/tomorrowli/dto/User.java
@@ -0,0 +1,18 @@
+package com.howie.akuoj.tomorrowli.dto;
+
+import lombok.Data;
+
+/**
+ * @Author: LiMing
+ * @Date: 2020/4/28 17:03
+ * @Description:
+ **/
+@Data
+public class User {
+
+
+    private String name;
+
+    private int age;
+
+}
