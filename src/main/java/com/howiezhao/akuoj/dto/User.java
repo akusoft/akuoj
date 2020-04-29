342907a943ceeef89ac2324106368c0fe83e525a
Tomorrowli <1425560783@qq.com>
2020-04-29 13:02:55 +0800

    测试master是否push成功1

--- src/main/java/com/howiezhao/akuoj/dto/User.java
+++ src/main/java/com/howiezhao/akuoj/dto/User.java
@@ -6,13 +6,32 @@ import lombok.Data;
  * @author LiMing
  * @since 2020/4/28 17:03
  **/
-@Data
 public class User {
-
-
     private String name;
-
-
     private int age;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
